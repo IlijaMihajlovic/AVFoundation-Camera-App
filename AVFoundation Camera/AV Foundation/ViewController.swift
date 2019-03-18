1b0f270b763f525ca691c3cdaaaeb1cf2fa540da
Ilija Mihajlovic <39649078+IlijaMihajlovic@users.noreply.github.com>
2019-03-18 22:09:40 +0100

    Add files via upload

---
+++ AVFoundation Camera/AV Foundation/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  AV Foundation
+//
+//  Created by Ilija Mihajlovic on 3/18/19.
+//  Copyright © 2019 Ilija Mihajlovic All rights reserved.
+//
+//
+
+import UIKit
+import Photos
+
+class ViewController: UIViewController {
+    
+    @IBOutlet fileprivate var captureButton: UIButton!
+    @IBOutlet fileprivate var capturePreviewView: UIView!
+    @IBOutlet fileprivate var photoModeButton: UIButton!
+    @IBOutlet fileprivate var toggleCameraButton: UIButton!
+    @IBOutlet fileprivate var toggleFlashButton: UIButton!
+    @IBOutlet fileprivate var videoModeButton: UIButton!
+    
+    override var prefersStatusBarHidden: Bool { return true }
+    
+    let cameraController = CameraController()
+
+    override func viewDidLoad() {
+        func configureCameraController() {
+            cameraController.prepare {(error) in
+                if let error = error {
+                    print(error)
+                }
+                
+                try? self.cameraController.displayPreview(on: self.capturePreviewView)
+            }
+        }
+        configureCameraController()
+    }
+
+}
+
+
+
+
+extension ViewController {
+    @IBAction func toggleFlash(_ sender: UIButton) {
+        if cameraController.flashMode == .on {
+            cameraController.flashMode = .off
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash Off Icon"), for: .normal)
+        }
+            
+        else {
+            cameraController.flashMode = .on
+            toggleFlashButton.setImage(#imageLiteral(resourceName: "Flash On Icon"), for: .normal)
+        }
+    }
+    
+  
+    
+    @IBAction func captureImage(_ sender: UIButton) {
+        sender.animate()
+        cameraController.captureImage {(image, error) in
+            guard let image = image else {
+                print(error ?? "Image capture error")
+                return
+            }
+            
+            try? PHPhotoLibrary.shared().performChangesAndWait {
+                PHAssetChangeRequest.creationRequestForAsset(from: image)
+            }
+        }
+    }
+    
+    
+    @IBAction func switchCameras(_ sender: UIButton) {
+        do {
+            try cameraController.switchCameras()
+        }
+            
+        catch {
+            print(error)
+        }
+        
+        switch cameraController.currentCameraPosition {
+        case .some(.front):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Front Camera Icon"), for: .normal)
+            
+        case .some(.rear):
+            toggleCameraButton.setImage(#imageLiteral(resourceName: "Rear Camera Icon"), for: .normal)
+            
+        case .none:
+            return
+        }
+    }
+    
+}
+
